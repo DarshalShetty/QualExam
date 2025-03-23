d8ede323edf79294d37ec6671c5bbfdaeabc2862
Darshal Shetty <darshalcs@gmail.com>
2025-03-23 00:43:17 -0400

    Add validators for germs and canonical terms

--- GCIC-implementation/evaluation.rkt
+++ GCIC-implementation/evaluation.rkt
@@ -4,7 +4,7 @@
                   supported-variants
                   Var Univ App Lam Pi IndT Constr IndElim
                   Unk Err Cast
-                  get-params
+                  get-params debug-log
                   ))
 (provide (all-defined-out))
 
@@ -56,10 +56,76 @@
      (Pi '_ (Unk (Univ (cΠ level))) (Unk (Univ (cΠ level))))]
     [(HeadPi) (Err (Univ level))]))
 
-;; Preconditons: (ccic-term? term) = #t
-(define (canonical-term? term)
-  (error 'canonical-term? "TODO: not implemented"))
+(define (germ? term level)
+  (match term
+    [(or (Univ i) (Err (Univ i)))
+     #:when (eqv? i level)
+     #t]
+    [(Pi _ (Unk (Univ i)) (Unk (Univ i)))
+     #:when (and (> (cΠ level) 0) (eqv? i (cΠ level)))
+     #t]
+    [(IndT _ i params)
+     #:when (eqv? i level)
+     (for/and ([param params])
+       (match param
+         [(Unk _) #t]
+         [_ #f]))]
+    [_
+     (debug-log (format "Not a germ at level ~a: ~a" level term))
+     #f]))
 
-;; Preconditons: (ccic-term? term) = #t
+;; Preconditons: (ccic-term? term)
+(define (canonical-term? term)
+  (match term
+    [(or (Lam _ _ _)
+         (Constr _ _ _ _ _)
+         (Pi _ _ _)
+         (Univ _)
+         (IndT _ _ _))
+     #t]
+    [(or (Unk t) (Err t))
+     (match t
+       [(or (Univ _)
+            (IndT _ _ _)
+            (Unk (Univ _))
+            (Err (Univ _)))
+        #t]
+       [_
+        (define-values (term-struct _) (struct-info t))
+        (define-values (term-name _1 _2 _3 _4 _5 _6 _7) (struct-type-info term-struct))
+        (debug-log (format "Invalid type associated with term ~a: ~a" term-name t))
+        #f])]
+    [(Cast term source (Unk (Univ level)))
+     #:when (germ? source level)
+     #t]
+    [_ (neutral-term? term)]))
+
+;; Preconditons: (ccic-term? term)
 (define (neutral-term? term)
-  (error 'neutral-term? "TODO: not implemented"))
+  (match term
+    [(Var _) #t]
+    [(App rator _) (neutral-term? rator)]
+    [(IndElim _ scrut _ _ _ _) (neutral-term? scrut)]
+    [(or (Unk t) (Err t)) (neutral-term? t)]
+    [(Cast source target term)
+     (match source
+       [(Unk (Univ _)) (neutral-term? term)]
+       [(Univ _) (neutral-term? target)]
+       [(Pi _ _ _)
+        (match target
+          [(Pi _ _ _) (neutral-term? term)]
+          [_ (neutral-term? target)])]
+       [(IndT _ _ _)
+        (match target
+          [(IndT _ _ _) (neutral-term? term)]
+          [_ (neutral-term? target)])]
+       [_ (neutral-term? source)])]
+    [_
+     (debug-log (format "Not a neutral term: ~a" term))
+     #f]))
+
+; ∃ scope, defs . (and (ccic-term? term defs scope)
+;                      (ccic-term? (reduce-principal term) defs scope))
+(define (reduce-principal term)
+  (match term
+    [_ (error 'reduce-principal "TODO: not implemented")]))
